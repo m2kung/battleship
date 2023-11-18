85ebebd8c4462495677d3429abd19cddbb7bad27
MoiraMcmurtrie <149909310+MoiraMcmurtrie@users.noreply.github.com>
2023-11-17 19:41:40 -0500

    Update Computer ship placement.py

--- Computer ship placement.py
+++ Computer ship placement.py
@@ -1,5 +1,5 @@
 import random as r
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 
 #This function checks to see if there are any duplicate sublists in a list
 def is_duplicate_double(list1, list2):
@@ -37,13 +37,13 @@ while unique2 == True:
             rand_letter = r.randrange(0,6)
             rand_number = str(r.randrange(1,11))
             while (i < 5):
-                computer_ship1 += [[alphabet[rand_letter + i] , rand_number]]
+                computer_ship1 += [[letters[rand_letter + i] , rand_number]]
                 i += 1
         else:
             rand_letter = r.randrange(0,10)
             rand_number = r.randrange(1,7)
             while (i < 5):
-                computer_ship1 += [[alphabet[rand_letter] , str(rand_number + i)]]
+                computer_ship1 += [[letters[rand_letter] , str(rand_number + i)]]
                 i +=1
         Computer_coordinates += computer_ship1
         
@@ -60,14 +60,14 @@ while unique2 == True:
                 rand_letter = r.randrange(0,7)
                 rand_number = str(r.randrange(1,11))
                 while (i < 4):
-                    computer_ship2 += [[alphabet[rand_letter + i] , rand_number]]
+                    computer_ship2 += [[letters[rand_letter + i] , rand_number]]
                     i += 1
                     j += 1
             else:
                 rand_letter = r.randrange(0,10)
                 rand_number = r.randrange(1,8)
                 while (i < 4):
-                    computer_ship2 += [[alphabet[rand_letter] , str(rand_number + i)]]
+                    computer_ship2 += [[letters[rand_letter] , str(rand_number + i)]]
                     i += 1
                     j += 1
             unique = is_duplicate_double(Computer_coordinates, computer_ship2)
@@ -85,14 +85,14 @@ while unique2 == True:
                 rand_letter = r.randrange(0,8)
                 rand_number = str(r.randrange(1,11))
                 while (i < 3):
-                    computer_ship3 += [[alphabet[rand_letter + i] , rand_number]]
+                    computer_ship3 += [[letters[rand_letter + i] , rand_number]]
                     i += 1
                     j += 1
             else:
                 rand_letter = r.randrange(0,10)
                 rand_number = r.randrange(1,9)
                 while (i < 3):
-                    computer_ship3 += [[alphabet[rand_letter] , str(rand_number + i)]]
+                    computer_ship3 += [[letters[rand_letter] , str(rand_number + i)]]
                     i += 1
                     j += 1
             unique = is_duplicate_double(Computer_coordinates,computer_ship3)
@@ -110,14 +110,14 @@ while unique2 == True:
                 rand_letter = r.randrange(0,8)
                 rand_number = str(r.randrange(1,11))
                 while (i < 3):
-                    computer_ship4 += [[alphabet[rand_letter + i] , rand_number]]
+                    computer_ship4 += [[letters[rand_letter + i] , rand_number]]
                     i += 1
                     j += 1
             else:
                 rand_letter = r.randrange(0,10)
                 rand_number = r.randrange(1,9)
                 while (i < 3):
-                    computer_ship4 += [[alphabet[rand_letter] , str(rand_number + i)]]
+                    computer_ship4 += [[letters[rand_letter] , str(rand_number + i)]]
                     i += 1
                     j += 1
             unique = is_duplicate_double(Computer_coordinates,computer_ship4)
@@ -135,14 +135,14 @@ while unique2 == True:
                 rand_letter = r.randrange(0,9)
                 rand_number = str(r.randrange(1,11))
                 while (i < 2):
-                    computer_ship5 += [[alphabet[rand_letter + i] , rand_number]]
+                    computer_ship5 += [[letters[rand_letter + i] , rand_number]]
                     i += 1
                     j += 1
             else:
                 rand_letter = r.randrange(0,10)
                 rand_number = r.randrange(1,10)
                 while (i < 2):
-                    computer_ship5 += [[alphabet[rand_letter] , str(rand_number + i)]]
+                    computer_ship5 += [[letters[rand_letter] , str(rand_number + i)]]
                     i += 1
                     j += 1
             unique = is_duplicate_double(Computer_coordinates,computer_ship5)
